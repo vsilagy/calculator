bdeefd8e14e5688b0d9e87bb7cb4445c1c215b96
Vlad Silagy <vladsilagy@gmail.com>
2022-08-11 15:08:00 -0400

    added buttons in javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".output");
+const keys = calculator.querySelector(".calc-keys");
+const numKeys = keys.querySelectorAll(".number");
