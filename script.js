edd2705b870c5e9d80cbea764d9e5de79cd328a4
Vlad Silagy <vladsilagy@gmail.com>
2022-08-17 19:05:51 -0400

    added calculate function using dataset attributes

--- script.js
+++ script.js
@@ -10,10 +10,49 @@ keys.addEventListener("click", (event) => {
   const key = event.target;
   const keyValue = key.textContent;
   const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
 
-  if (displayValue === "0") {
-    display.textContent = keyValue;
-  } else {
-    display.textContent = displayValue + keyValue;
+  if (type === "number") {
+    if (displayValue === "0" || previousKeyType === "operator") {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
   }
+
+  if (type === "operator") {
+    operationKeys.forEach((e) => {
+      e.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "equal") {
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+    display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  if (type === "clear") {
+    display.textContent = "0";
+    delete calculator.dataset.firstNumber;
+    delete calculator.dataset.operator;
+  }
+
+  calculator.dataset.previousKeyType = type;
 });
+
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  if (operator === "add") return firstNumber + secondNumber;
+  if (operator === "subtract") return firstNumber - secondNumber;
+  if (operator === "multiply") return firstNumber * secondNumber;
+  if (operator === "divide") return firstNumber / secondNumber;
+}
