4067dad9bc2549b0f61e297736701a48ee25a703
Vlad Silagy <vladsilagy@gmail.com>
2022-08-16 23:59:40 -0400

    updated the style

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const calculator = document.querySelector(".calculator");
 const display = calculator.querySelector(".display");
 const keys = calculator.querySelector(".calc-keys");
-const numberButtons = keys.querySelectorAll("[data-number]");
-const operationButtons = keys.querySelectorAll("[data-operation]");
-const equalsButton = keys.querySelector("[data-equals]");
-const deleteButton = keys.querySelector("[data-delete]");
-const resetButton = keys.querySelector("[data-reset]");
+const operationKeys = keys.querySelectorAll('[data-type="operator"]');
+const clearKey = keys.querySelector('[data-type="clear"]');
 
 keys.addEventListener("click", (event) => {
   if (!event.target.closest("button")) return;
