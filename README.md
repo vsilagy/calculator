e41f3ded35832add2c6500921f7ffb6ed08aec3d
Vlad Silagy <vladsilagy@gmail.com>
2022-08-23 02:04:46 -0400

    updating the calculator script from using classes to functions

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # **The Odin Project**
 
-## Project : [Calculator](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator)
+## Foundations Project : [Calculator](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator)
 
 [Live Preview](https://vsilagy.github.io/calculator)
 
 - Build a calculator using JavaScript
 - Dev branch is built using Classes and data-attributes
-
 - For the UI I used [Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29)
--- script.js
+++ script.js
@@ -1,108 +1,32 @@
-class Calculator {
-  constructor(previousOperandTextEl, currentOperandTextEl) {
-    this.previousOperandTextEl = previousOperandTextEl;
-    this.currentOperandTextEl = currentOperandTextEl;
-    this.reset();
-  }
-
-  reset() {
-    this.currentOperand = "";
-    this.previousOperand = "";
-    this.operation = undefined;
-  }
-
-  delete() {
-    this.currentOperand = this.currentOperand.toString().slice(0, -1);
-  }
-
-  appendNumber(number) {
-    if (number === "." && this.currentOperand.includes(".")) return;
-    this.currentOperand = this.currentOperand.toString() + number.toString();
-  }
-
-  chooseOperation(operation) {
-    if (this.currentOperand === "") return;
-    if (this.currentOperand !== "") {
-      this.compute();
-    }
-    this.operation = operation;
-    this.previousOperand = this.currentOperand;
-    this.currentOperand = "";
-  }
-
-  compute() {
-    let computation;
-    const prev = parseFloat(this.previousOperand);
-    const current = parseFloat(this.currentOperand);
-    if (isNaN(prev) || isNaN(current)) return;
-    switch (this.operation) {
-      case "+":
-        computation = prev + current;
-        break;
-      case "-":
-        computation = prev - current;
-        break;
-      case "×":
-        computation = prev * current;
-        break;
-      case "÷":
-        computation = prev / current;
-        break;
-      default:
-        return;
-    }
-
-    this.currentOperand = computation;
-    this.operation = undefined;
-    this.previousOperand = "";
-  }
-
-  updateDisplay() {
-    this.currentOperandTextEl.innerText = this.currentOperand;
-    if (this.operation != null) {
-      this.previousOperandTextEl.innerText = `${this.previousOperand} ${this.operation}`;
-    } else {
-      this.previousOperandTextEl.innerText = "";
-    }
-  }
-}
-
 const calcKeys = document.querySelector(".calc-keys");
 const numKeys = calcKeys.querySelectorAll("[data-number]");
 const operationKeys = calcKeys.querySelectorAll("[data-operation]");
 const equalsKey = calcKeys.querySelector("[data-equals]");
 const deleteKey = calcKeys.querySelector("[data-delete]");
 const resetKey = calcKeys.querySelector("[data-reset]");
-const previousOperandTextEl = document.querySelector("[data-previous-operand]");
-const currentOperandTextEl = document.querySelector("[data-current-operand]");
-
-const calculator = new Calculator(previousOperandTextEl, currentOperandTextEl);
+const previousDisplay = document.querySelector("[data-previous-display]");
+const currentDisplay = document.querySelector("[data-current-display]");
 
 numKeys.forEach((key) => {
-  key.addEventListener("click", () => {
-    calculator.appendNumber(key.innerText);
-    calculator.updateDisplay();
+  key.addEventListener("click", (e) => {
+    console.log(key.innerText);
   });
 });
 
 operationKeys.forEach((key) => {
   key.addEventListener("click", () => {
-    calculator.chooseOperation(key.innerText);
-    calculator.updateDisplay();
+    console.log(key.innerText);
   });
 });
 
 equalsKey.addEventListener("click", (key) => {
-  calculator.compute();
-  calculator.updateDisplay();
+  
 });
 
 resetKey.addEventListener("click", (key) => {
-  calculator.reset();
-  calculator.updateDisplay();
+  
 });
 
 deleteKey.addEventListener("click", (key) => {
-  calculator.delete();
-  calculator.updateDisplay();
+  
 });
