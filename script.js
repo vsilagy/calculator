b40fc197c76e078517223d5b2141e2ad3d2b1973
Vlad Silagy <vladsilagy@gmail.com>
2022-08-23 02:45:25 -0400

    delete and decimal buttons working

--- script.js
+++ script.js
@@ -1,91 +1,99 @@
 const calcKeys = document.querySelector(".calc-keys");
 const numKeys = calcKeys.querySelectorAll("[data-number]");
-const operationKeys = calcKeys.querySelectorAll("[data-operation]");
+const operatorKeys = calcKeys.querySelectorAll("[data-operation]");
 const equalsKey = calcKeys.querySelector("[data-equals]");
-const deleteKey = calcKeys.querySelector("[data-delete]");
+const delKey = calcKeys.querySelector("[data-delete]");
 const resetKey = calcKeys.querySelector("[data-reset]");
 const previousDisplay = document.querySelector("[data-previous-display]");
 const currentDisplay = document.querySelector("[data-current-display]");
 
 let operator = "";
-let currentValue = "";
-let previousValue = "";
+let currentNum = "";
+let previousNum = "";
 
 numKeys.forEach((key) => {
-  key.addEventListener("click", () => {
-    handleNumber(key.innerText);
-    currentDisplay.textContent = currentValue;
+  key.addEventListener("click", (e) => {
+    handleNumber(e.target.innerText);
+    currentDisplay.textContent = currentNum;
+
+    if (e.target.innerText === ".") {
+      handleDecimal()
+    }
   });
 });
 
-operationKeys.forEach((key) => {
-  key.addEventListener("click", () => {
-    handleOperator(key.innerText);
-    previousDisplay.textContent = `${previousValue} ${operator}`;
-    currentDisplay.textContent = currentValue;
+operatorKeys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    handleOperator(e.target.innerText);
+    previousDisplay.textContent = `${previousNum} ${operator}`;
+    currentDisplay.textContent = currentNum;
   });
 });
 
 equalsKey.addEventListener("click", () => {
-  if (currentValue !== "" && previousValue !== "") {
+  if (currentNum !== "" && previousNum !== "") {
     calculate();
     previousDisplay.textContent = "";
-    currentDisplay.textContent = currentValue;
+    currentDisplay.textContent = currentNum;
   }
 });
 
 resetKey.addEventListener("click", () => {
-  previousValue = "";
-  currentValue = "";
+  previousNum = "";
+  currentNum = "";
   operator = "";
-  previousDisplay.textContent = currentValue;
-  currentDisplay.textContent = currentValue;
+  previousDisplay.textContent = currentNum;
+  currentDisplay.textContent = currentNum;
 });
 
-deleteKey.addEventListener("click", () => {
-
+delKey.addEventListener("click", () => {
+  handleDelete();
+  currentDisplay.textContent = currentNum;
 });
 
 
 function calculate() {
   let computation;
-  let prev = parseFloat(previousValue);
-  let current = parseFloat(currentValue);
-  if (isNaN(prev) || isNaN(current)) return;
+  let prev = parseFloat(previousNum);
+  let current = parseFloat(currentNum);
+  // if (isNaN(prev) || isNaN(current)) return;
   switch (operator) {
-          case "+":
-            computation = prev + current;
-            break;
-          case "-":
-            computation = prev - current;
-            break;
-          case "×":
-            computation = prev * current;
-            break;
-          case "÷":
-            computation = prev / current;
-            break;
-          default:
-            return;
-        }
-        currentValue = computation;
-        previousValue = "";
-        operator = "";
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "×":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+  currentNum = computation;
+  previousNum = "";
+  operator = "";
 }
 
 function handleNumber(e) {
-  currentValue += e;
+  currentNum += e;
 }
 
 function handleOperator(e) {
   operator = e;
-  previousValue = currentValue;
-  currentValue = ""
+  previousNum = currentNum;
+  currentNum = ""
 }
 
 function handleDelete() {
+  currentNum = currentNum.toString().slice(0, -1);
 }
 
 function handleDecimal() {
-  
+  if (!currentNum.includes) {
+    currentNum = currentNum.toString() + ".";
+  }
 }
